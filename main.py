b2234f533d6c64974b5313eb5b75e5274cfd4975
cs <cs010@hotmail.com>
2019-07-29 16:24:26 +0800

    flags=pytun.IFF_TUN | pytun.IFF_NO_PI
    platform

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from tun2socks.tcp_handler import ConnectionHandler
 
 def start():
     loop = asyncio.get_event_loop()
-    tun = pytun.TunTapDevice(dev="8")
+    tun = pytun.TunTapDevice(dev="8", name="tun2socks", flags=pytun.IFF_TUN | pytun.IFF_NO_PI)
     tun.set(addr="12.0.0.1", dstaddr="12.0.0.20", netmask="255.255.255.0", mtu=1500, hwaddr="")
     tun.up()
     tun2socks = Tun2Socks(tun, ConnectionHandler, loop)
