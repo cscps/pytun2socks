1a8d22256251bb59dfa183cda24feea770a80135
cs <cs010@hotmail.com>
2019-08-14 17:44:31 +0800

    fix sys.argv in embbed

--- embbed.c
+++ embbed.c
@@ -5,10 +5,20 @@
 
 void PyInit_tabinit();
 
-int main (void){
+int main (int argc, char** argv){
     PyInit_tabinit();
     Py_Initialize();
-    PyRun_SimpleString("import sys;sys.path.append('.'); print(1)");
+    wchar_t* wchar_argv[argc];
+    for (int i=0; i<argc; i++){
+        size_t str_len = strlen(argv[i])+1;
+        wchar_t* wt = malloc(sizeof(wchar_t) * str_len);
+        for (int j=0; j<str_len; j++){
+            wt[j] = argv[i][j];
+        }
+        wchar_argv[i] = wt;
+    }
+    if (argv) PySys_SetArgv(argc, wchar_argv);
+    PyRun_SimpleString("import sys;sys.path.append('.'); print(sys.argv)");
     PyRun_SimpleString("import main; main.start()");
     return 0;
 }
